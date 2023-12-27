2885f6018c649a72f7bdf2c601362a2a3164938a
Telvo <telvowarrior@gmail.com>
2023-12-27 15:35:35 +0300

    add about function

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is Home page.")
 }
 
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is About page.")
+}
+
 func main() {
 	
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	http.ListenAndServe(":8080", nil)
 
